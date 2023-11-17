7e1fde7c3054633708fff4284f52d8115aa4351b
brycebien <brycebien@gmail.com>
2023-11-17 00:12:11 -0500

    high score writes to highscore.txt to store high score -- feature complete

--- tetris.py
+++ tetris.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 import random
 import sound
@@ -176,11 +177,20 @@ class HighScore:
     def __init__(self):
         self.score = 0
         #TODO: write hs to file when it is > 0/>old hs -- grab hs from file
-        self.high_score = 0
+        if os.path.exists('highscore.txt'):
+            with open ('highscore.txt', 'r') as file:
+                self.high_score = int(file.read())
+        else:
+            with open('highscore.txt', 'w') as file:
+                file.write('0')
+                self.high_score = 0
+
 
     def _check_highscore(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open ('highscore.txt', 'w') as file:
+                file.write(str(self.high_score))
 
     def update_score(self, amnt):
         self.score += amnt
